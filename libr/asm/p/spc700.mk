f9ed78139da530931298eb0c56e84fe65edd11a3
Anton Kochkov <anton.kochkov@gmail.com>
2021-06-07 17:39:09 +0800

    Remove SPC700

--- libr/asm/p/spc700.mk
+++
@@ -1,9 +0,0 @@
-OBJ_SPC700=asm_spc700.o
-
-STATIC_OBJ+=${OBJ_SPC700}
-TARGET_SPC700=asm_spc700.${LIBEXT}
-
-ALL_TARGETS+=${TARGET_SPC700}
-
-${TARGET_SPC700}: ${OBJ_SPC700}
-	${CC} $(call libname,asm_spc700) ${CFLAGS} -o ${TARGET_SPC700} ${OBJ_SPC700} -lr_asm -lr_util
